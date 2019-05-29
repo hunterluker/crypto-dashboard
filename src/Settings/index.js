f546a921900a2435f5c5ac04bc91c17137930fc9
hunterluker <hunterluker1992@gmail.com>
2019-05-29 09:15:13 -0600

    Add Page

---
+++ src/Settings/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Welcome from './WelcomeMessage';
+import ConfirmButton from './ConfirmButton';
+import Page from '../Shared/Page';
+
+export default function() {
+  return (
+    <Page name="settings">
+      <Welcome />
+      <ConfirmButton />
+    </Page>
+  );
+}
