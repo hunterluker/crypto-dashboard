a6b1eb5ce544bc8f9c00ed2ed6bfc1c37a02a9ce
hunterluker <hunterluker1992@gmail.com>
2019-05-28 21:26:37 -0600

    Add App Layout

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import AppLayout from './AppLayout';
+
+function App() {
+  return (
+    <AppLayout>
+      <WelcomeMessage />
+    </AppLayout>
+  );
+}
+
+export default App;
